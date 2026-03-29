ebb5292cbdc5a1fa93f676806af5b406931811f4
ilyxas <ilyxas@gmail.com>
2026-03-29 03:38:55 +0300

    add llm basic

---
+++ UIBuilder/Engine/LLMBasic/LocalLLMView.swift
@@ -0,0 +1,87 @@
+// Copyright © 2025 Apple Inc.
+
+import MLXLMCommon
+import SwiftUI
+import MLX
+import MLXLLM
+
+struct LocalLLMView: View {
+
+    init() {
+        Memory.cacheLimit = 20 * 1024 * 1024
+    }
+    
+    @State var loader = ModelLoader()
+
+    /// once loaded this will hold the chat session
+    @State var session: ChatModel?
+    @State var error: String?
+
+    /// prompt for the LLM (text field)
+    @State var prompt = ""
+
+    @FocusState var promptFocused
+
+    var body: some View {
+        VStack {
+            if let error {
+                Text("Error: \(error)")
+
+            } else if !loader.isLoaded {
+                ProgressView("Loading", value: loader.progress, total: 1)
+
+            } else if let session {
+                // show the chat messages
+                ScrollView(.vertical) {
+                    ForEach(session.messages.enumerated(), id: \.offset) { _, message in
+                        let bold = message.role == .user
+
+                        HStack {
+                            Text(message.content)
+                                .bold(bold)
+                            Spacer()
+                        }
+                        .padding(.bottom, 4)
+                    }
+
+                    Spacer()
+
+                    if session.isBusy {
+                        // a stop button -- cmd-. to interrupt
+                        HStack {
+                            Button("Stop", action: { session.cancel() })
+                                .keyboardShortcut(".")
+                            Spacer()
+                        }
+                    } else {
+                        // message from the user -> LLM
+                        TextField("Prompt", text: $prompt)
+                            .onSubmit(respond)
+                            .focused($promptFocused)
+                            .onAppear {
+                                promptFocused = true
+                            }
+                    }
+                }
+                .defaultScrollAnchor(.bottom)
+            }
+        }
+        .padding()
+        .task {
+            do {
+                let model = try await loader.model()
+                self.session = ChatModel(model: model)
+            } catch {
+                self.error = error.localizedDescription
+            }
+        }
+        .onDisappear {
+            self.session?.cancel()
+        }
+    }
+
+    private func respond() {
+        session?.respond(prompt)
+        prompt = ""
+    }
+}
