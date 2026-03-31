dc0163271c9f524e0570b0ca9c3fab1c14a7cb7c
ilyxas <ilyxas@gmail.com>
2026-03-31 23:36:27 +0300

    Refactor LLM Model and chat session baseline

---
+++ UIBuilder/App/LocalLLMView.swift
@@ -0,0 +1,130 @@
+// Copyright © 2025 Apple Inc.
+
+import MLXLMCommon
+import SwiftUI
+import MLX
+import MLXLLM
+
+struct LocalLLMView: View {
+
+    init(llm: Binding<LLMEvaluator>) {
+        Memory.cacheLimit = 20 * 1024 * 1024
+        self._llm = llm
+    }
+    
+    enum DisplayStyle: String, CaseIterable, Identifiable {
+        case plain, markdown
+        var id: Self { self }
+    }
+    
+    @Environment(DeviceStat.self) private var deviceStat
+    
+    @State private var selectedDisplayStyle = DisplayStyle.markdown
+    
+    //@State var loader = ModelLoader()
+    
+    @State var llm: LLMEvaluator
+
+    /// once loaded this will hold the chat session
+    @State var session: ChatModel?
+    @State var error: String?
+
+    /// prompt for the LLM (text field)
+    @State var prompt = ""
+
+    @FocusState var promptFocused
+
+    var body: some View {
+        VStack {
+                // Header Section
+                HeaderView(
+                    llm: llm,
+                    selectedDisplayStyle: $selectedDisplayStyle
+                )
+            
+
+                if let session {
+                // show the chat messages
+                ScrollView(.vertical) {
+                    VStack(alignment: .leading, spacing: 0) {
+                        ForEach(session.messages.enumerated(), id: \.offset) { _, message in
+                            HStack {
+                                OutputViewSecond(
+                                    output: message.content,
+                                    displayStyle: selectedDisplayStyle,
+                                    wasTruncated: llm.wasTruncated
+                                )
+                            }
+                            .textSelection(.enabled)
+                            .padding(.bottom, 4)
+                        }
+
+                        Spacer()
+
+                        if session.isBusy {
+                            HStack {
+                                Button("Stop", action: { session.cancel() })
+                                    .keyboardShortcut(".")
+                                Spacer()
+                            }
+                        } else {
+                            TextField("Prompt", text: $prompt)
+                                .onSubmit(respond)
+                                .focused($promptFocused)
+                                .onAppear {
+                                    promptFocused = true
+                                }
+                        }
+                    }
+                    .textSelection(.enabled)
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        promptFocused = false
+                    }
+                }
+                .scrollDismissesKeyboard(.interactively)
+                .defaultScrollAnchor(.bottom)
+            }
+        }
+        .padding()
+        
+//        // Performance Metrics Panel
+//        MetricsView(
+//            tokensPerSecond: llm.tokensPerSecond,
+//            timeToFirstToken: llm.timeToFirstToken,
+//            promptLength: llm.promptLength,
+//            totalTokens: llm.totalTokens,
+//            totalTime: llm.totalTime,
+//            memoryUsed: deviceStat.gpuUsage.activeMemory,
+//            cacheMemory: deviceStat.gpuUsage.cacheMemory,
+//            peakMemory: deviceStat.gpuUsage.peakMemory
+//        )
+            
+        
+        .task {
+            do {
+                let model = try await llm.load()
+                self.session = ChatModel(model: model, genParameters: llm.generateParameters)
+            } catch {
+                self.error = error.localizedDescription
+            }
+        }
+        .onDisappear {
+            //self.session?
+        }
+        .overlay {
+            if llm.isLoading {
+                LoadingOverlayView(
+                    modelInfo: llm.modelInfo,
+                    downloadProgress: llm.downloadProgress,
+                    progressDescription: llm.totalSize
+                )
+            }
+        }
+    }
+
+    private func respond() {
+        session?.respond(prompt)
+        prompt = ""
+    }
+}
